9e94ab33704c630b9570555d8e7ebbc26fe4a80a
Maurice Freuwört <maurice.freuwoert@gmail.com>
2024-01-10 14:13:36 +0100

    Refactor personal settings page

--- pages/d/settings/personal.vue
+++ pages/d/settings/personal.vue
@@ -1,13 +1,17 @@
 <template>
-    <h5 class="margin-0 weight-medium">Sprache, Zeitzone und Theme</h5>
+    <Flex horizontal>
+        <h5 class="margin-0 weight-medium">Sprache, Zeitzone und Theme</h5>
+        <Spacer />
+        <IodLoader v-if="processing" class="margin-left-1"/>
+    </Flex>
     <Flex horizontal>
         <Flex class="flex-1">
             <span class="font-heading color-text weight-medium">Sprache</span>
             <small>Wählen Sie die Programm Sprache</small>
         </Flex>
-        <select class="w-18" v-model="form.language">
+        <select class="w-18" v-model="form.language" @input="updateSetting('language', form.language)">
             <option value="de">Deutsch</option>
-            <option value="en" disabled>Englisch</option>
+            <option value="en">Englisch</option>
         </select>
     </Flex>
     <Flex horizontal>
@@ -15,22 +19,11 @@
             <span class="font-heading color-text weight-medium">Zeitzone</span>
             <small>Wählen Sie Ihre Zeitzone</small>
         </Flex>
-        <select class="w-18" v-model="form.timezone">
-            <option value="pst">PST (UTC-8)</option>
-            <option value="mst">MST (UTC-7)</option>
-            <option value="cst">CST (UTC-6)</option>
-            <option value="est">EST (UTC-5)</option>
-            <option value="ast">AST (UTC-4)</option>
-            <option value="brt">BRT (UTC-3)</option>
-            <option value="gmt">GMT (UTC+0)</option>
-            <option value="cet">CET (UTC+1)</option>
-            <option value="eet">EET (UTC+2)</option>
-            <option value="msk">MSK (UTC+3)</option>
-            <option value="ist">IST (UTC+5:30)</option>
-            <option value="sgt">SGT (UTC+8)</option>
-            <option value="jst">JST (UTC+9)</option>
-            <option value="aest">AEDT (UTC+11)</option>
-            <option value="nzst">NZDT (UTC+13)</option>
+        <select class="w-18" v-model="form.timezone" @input="updateSetting('timezone', form.timezone)">
+            <option value="America/Los_Angeles">Los Angeles</option>
+            <option value="America/New_York">New York</option>
+            <option value="Europe/Berlin">Berlin</option>
+            <option value="Pacific/Auckland">New Zealand</option>
         </select>
     </Flex>
     <Flex horizontal>
@@ -38,7 +31,7 @@
             <span class="font-heading color-text weight-medium">Theme</span>
             <small>Wählen Sie Ihr lieblings Theme</small>
         </Flex>
-        <select class="w-18" v-model="form.theme">
+        <select class="w-18" v-model="form.theme" @input="updateSetting('theme', form.theme)">
             <option value="light">Hell</option>
             <option value="dark">Dunkel</option>
             <option value="system">System</option>
@@ -76,11 +69,28 @@
 </template>
 
 <script lang="ts" setup>
-    const form = ref({
+    const processing = ref(false)
+    const form = reactive({
         language: 'de',
-        timezone: 'cet',
-        theme: 'light',
+        timezone: 'Europe/Berlin',
+        theme: 'light'
     })
+
+    async function updateSetting(key: string, value: any)
+    {
+        processing.value = true
+
+        await useApiFetch('/sanctum/csrf-cookie')
+
+        await useApiFetch('/api/user/settings', {
+            method: 'PATCH',
+            body: {
+                [key]: value
+            }
+        })
+
+        processing.value = false
+    }
 </script>
 
 <style lang="sass" scoped></style>
