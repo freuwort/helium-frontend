faf82224fa707f7d5d805f46ce76fc23efb03973
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-09-19 15:15:06 +0200

    Remove unused app.config.ts Remove comments from nuxt.config.ts

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineAppConfig({
-    appName: 'Helium',
-    appShortName: 'Helium',
-    appDescription: 'Das einfache ERP-System',
-    appIcon: '',
-    appThemeColor: '#3737FA',
-    appLanguage: 'de',
-})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,13 +29,9 @@ export default defineNuxtConfig({
 
     runtimeConfig: {
         public: {
-            // NUXT_PUBLIC_BACKEND_URL
             backendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.BACKEND_DOMAIN ?? 'localhost:8000'}`,
-            // NUXT_PUBLIC_FRONTEND_URL
             frontendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.FRONTEND_DOMAIN ?? 'localhost:3000'}`,
-            // NUXT_PUBLIC_WEBSITE_URL
             websiteUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.WEBSITE_DOMAIN ?? 'localhost:3001'}`,
-            // NUXT_PUBLIC_SCREENS_URL
             screensUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.SCREENS_DOMAIN ?? 'localhost:3002'}`,
             adminPermissions: ['system.super-admin', 'system.admin'],
             superAdminPermissions: ['system.super-admin'],
