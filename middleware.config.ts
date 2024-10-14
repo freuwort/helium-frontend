7f4479f283ffe6c625319818edab4cdb03cab709
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-10-14 10:00:56 +0200

    Rename 2fa verified gate

--- middleware.config.ts
+++ middleware.config.ts
@@ -4,7 +4,7 @@ export default {
         '/register':            ['guest'],
         '/forgot-password':     ['guest'],
         '/email-not-verified':  ['auth', 'email-not-verified'],
-        '/verify-2fa':          ['auth', 'email-verified', '2fa-needed'],
+        '/verify-2fa':          ['auth', 'email-verified', '2fa-not-verified'],
         '/disabled':            ['auth', 'email-verified', '2fa-verified', 'disabled'],
         '/change-password':     ['auth', 'email-verified', '2fa-verified', 'enabled', 'require-password-change'],
         '/setup-2fa':           ['auth', 'email-verified', '2fa-verified', 'enabled', 'dont-require-password-change', 'require-two-factor-setup'],
