ad5c6eb6365be8f10b3456e66fb6c9ce03b6a5d3
Alyx <33777960+freuwoert@users.noreply.github.com>
2024-11-10 03:03:08 +0100

    Add accounting contacts overview and editor

---
+++ pages/accounting/contacts/editor/[[id]].vue
@@ -0,0 +1,194 @@
+<template>
+    <NuxtLayout name="auth-default" limiter="medium" :scope pageTitle="Kontakt Editor" color="#FF4757">
+        <HeCard is="form" @submit.prevent="save">
+            <div class="flex items-center p-4 rounded-t-2xl border-b sticky top-16 z-20 bg-background">
+                <IodButton :is="NuxtLink" corner="pill" label="Zur Übersicht" variant="contained" to="/accounting/contacts"/>
+                <HeSpacer />
+                <IodButton type="submit" corner="pill" label="Speichern" :loading="form.processing" variant="filled" />
+            </div>
+
+            <HeFlex padding="1.5rem 1rem" :gap="3">
+                <ErrorAlert :errors="form.errors" />
+
+                <HeFlex :gap="1">
+                    <IodSelect label="Typ" v-model="form.type" :options="typeOptions"/>
+                    <IodInput label="Name" v-model="form.name"/>
+                    <IodInput label="Kontakt Person" v-model="form.contact_person"/>
+                </HeFlex>
+
+                <HeFlex :gap="1">
+                    <IodInput label="USt-IdNr." v-model="form.vat_id"/>
+                    <IodInput label="Steuernummer" v-model="form.tax_id"/>
+                </HeFlex>
+
+                <HeFlex :gap="1" v-show="form.type !== 'self'">
+                    <IodInput label="Kundennummer" v-model="form.customer_id" v-show="form.type === 'customer'"/>
+                    <IodInput label="Lieferantennummer" v-model="form.supplier_id" v-show="form.type === 'supplier'"/>
+                    <IodInput label="Mitarbeiternummer" v-model="form.employee_id" v-show="form.type === 'employee'"/>
+                </HeFlex>
+
+
+
+                <!-- <HeFlex :gap="1">
+                    <HeFlex horizontal>
+                        <h5 class="m-0 font-medium">Adressen</h5>
+                        <HeSpacer />
+                        <IodButton type="button" label="Neue Adresse" size="s" corner="pill" variant="contained" @click="addAddress()"/>
+                    </HeFlex>
+    
+                    <div class="entity-grid" v-if="form.addresses.length">
+                        <HeCard class="entity-card" v-for="address, i in form.addresses">
+                            <HeFlex class="entity-card-head" padding="1rem">
+                                <IodIcon icon="location_on" />
+                                <IodButton type="button" label="Löschen" size="s" corner="pill" variant="contained" color-preset="error" @click="removeAddress(i)"/>
+                            </HeFlex>
+                            <HeFlex padding="1rem" gap="1rem">
+                                <IodInput v-model="address.address_line_1" label="Straße" />
+                                <IodInput v-model="address.postal_code" label="Postleitzahl" />
+                                <IodInput v-model="address.city" label="Stadt" />
+                                <IodSelect v-model="address.country_code" label="Land" :options="countries.map((e: Country) => ({text: e.name, value: e.code}))"/>
+                            </HeFlex>
+                        </HeCard>
+                    </div>
+
+                    <IodAlert type="placeholder" class="h-40" text="Es wurden noch keine Adressen angelegt" v-else />
+                </HeFlex> -->
+            </HeFlex>
+        </HeCard>
+    </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+    import { toast } from 'vue3-toastify'
+
+    const NuxtLink = defineNuxtLink({})
+    const scope = 'view_admin_users_show'
+
+    
+
+    // Item id
+    const id = computed(() => useRoute().params.id ?? null)
+
+    // Save function
+    const save = id.value ? update : store
+
+
+
+    const form = useForm({
+        id: id.value,
+        type: 'customer',
+        name: '',
+        vat_id: '',
+        tax_id: '',
+        customer_id: '',
+        supplier_id: '',
+        employee_id: '',
+        contact_person: '',
+        created_at: '',
+        updated_at: '',
+        main_address: null,
+        billing_address: null,
+        shipping_address: null,
+    })
+
+
+
+    // START: Types
+    const typeOptions = [
+        { text: 'Kunde', value: 'customer' },
+        { text: 'Lieferant', value: 'supplier' },
+        { text: 'Mitarbeiter', value: 'employee' },
+        { text: 'Eigenkontakt', value: 'self' },
+    ]
+    // END: Types
+
+
+
+    // START: Addresses
+    function addAddress() {
+        form.addresses.push({
+            id: null,
+            address_line_1: '',
+            address_line_2: '',
+            postal_code: '',
+            city: '',
+            state: '',
+            country_code: '',
+            notes: '',
+        })
+    }
+
+    function removeAddress(index: number) {
+        form.addresses.splice(index, 1)
+    }
+    // END: Addresses
+
+
+
+    // START: Server routes
+    function fetch()
+    {
+        form.get(apiRoute('/api/users/:id', { id: id.value }), {
+            onSuccess(response: any)
+            {
+                form.defaults(response.data).reset()
+            },
+        })
+    }
+
+    function store()
+    {
+        form.post(apiRoute('/api/users'), {
+            onSuccess(response: any)
+            {
+                form.defaults(response.data).reset()
+                toast.success('Nutzer wurde erstellt')
+                navigateTo(apiRoute('/users/editor/:id', { id: response.data?.id }))
+            },
+        })
+    }
+
+    function update()
+    {
+        form.patch(apiRoute('/api/users/:id', { id: id.value }), {
+            onSuccess(response: any)
+            {
+                form.defaults(response.data).reset()
+                toast.success('Nutzer wurde aktualisiert')
+            },
+        })
+    }
+    // END: Server routes
+
+    
+    
+    // Fetch model
+    if (id.value) fetch()
+</script>
+
+<style lang="sass" scoped>
+    .entity-grid
+        display: grid
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr))
+        gap: 2rem
+
+    .entity-card
+        display: flex
+        flex-direction: column
+        align-items: stretch
+        overflow: hidden
+        box-shadow: none !important
+
+        .entity-card-head
+            position: relative
+            height: 10rem
+            background: var(--color-background-soft)
+
+        .iod-button
+            position: absolute
+            top: 1rem
+            right: 1rem
+
+        .iod-icon
+            font-size: 4rem
+</style>
---
+++ pages/accounting/contacts/index.vue
@@ -0,0 +1,238 @@
+<template>
+    <NuxtLayout name="auth-default" :scope :pageTitle="IPM.options.pageTitle" color="#FF4757">
+        <HeCard>
+            <IodTable
+                class="top-16"
+                :scope
+                :columns="tableColumns"
+                :actions="tableActions"
+                :filterSettings="tableFilters"
+                :items="IPM.items"
+                :loading="IPM.processing"
+                v-model:selection="IPM.selection"
+                v-model:filter="IPM.modelFilter"
+                v-model:sort="IPM.modelSort"
+                v-model:pagination="IPM.modelPagination"
+                v-model:columnSettings="IPM.modelColumnSettings"
+                @request:refresh="IPM.fetch()"
+            >
+                <template #right>
+                    <IodButton type="button" label="Neuer Kontakt" corner="pill" icon-right="add" @click="IPM.open()"/>
+                </template>
+
+                <template #wrapped-right>
+                    <IodIconButton type="button" size="s" corner="pill" variant="text" icon="cloud_upload" @click="importPopup.select()" v-tooltip="'Importieren'"/>
+                    <!-- <IodIconButton type="button" size="s" corner="pill" variant="text" icon="file_save" @click="true" v-tooltip="'Exportieren'"/> -->
+                    <!-- <IodIconButton type="button" size="s" corner="pill" variant="text" icon="stacked_email" @click="sendEmailPopup.open(IPM.keys)" v-tooltip="'Massenmail versenden'"/> -->
+                </template>
+            </IodTable>
+        </HeCard>
+
+        <DialogCsvImport ref="importPopup" :fields="importFields" @import="importUsers" />
+    </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+    import type { FilterSetting } from '~/components/Iod/IodTable.vue'
+    import { FieldGroup, Field } from '~/classes/import/CsvImport'
+    import TableColumnBuilder from '~/classes/Builder/TableColumnBuilder'
+    import { toast } from 'vue3-toastify'
+    
+    const dayjs = useDayjs()
+    const scope = 'view_admin_accounting_contact_index'
+
+
+
+    const IPM = useItemPageManager({
+        scope,
+        pageTitle: 'Kontakte Verwaltung',
+        routes: {
+            fetch: '/api/accounting/contacts',
+            duplicate: '/api/accounting/contacts/:id/duplicate',
+            delete: '/api/accounting/contacts',
+            editor: '/accounting/contacts/editor/:id',
+        },
+    })
+    
+    const tableColumnBuilder = new TableColumnBuilder()
+    const tableColumns = [
+        tableColumnBuilder.new().name('id').label('ID').width(70).build(),
+        tableColumnBuilder.new().name('name').label('Name').transform((value: string | null, item: any) => ({
+            text: value || '-',
+            tooltip: value || '-',
+            image: item.avatar,
+            icon: 'person',
+        })).build(),
+        tableColumnBuilder.new().name('customer_id').label('Kunden-Nr.').build(),
+        tableColumnBuilder.new().name('employee_id').label('Personal-Nr.').build(),
+        tableColumnBuilder.new().name('member_id').label('Mitglieds-Nr.').build(),
+        tableColumnBuilder.new().name('username').label('Nutzername').build(),
+        tableColumnBuilder.new().name('email').label('Email').build(),
+        tableColumnBuilder.new().name('phone').label('Telefon').build(),
+        tableColumnBuilder.new().name('roles').label('Rollen').transform((value: any[]) => value?.map(e => e.name)?.join(', ') || null).build(),
+        tableColumnBuilder.new().name('is_admin').label('Berechtigungslevel').sortable(false).transform((value: boolean | null, item: any) => {
+            if (item?.is_admin) return { text: 'Admin', icon: 'shield', color: 'var(--color-info)', }
+            if (item?.has_elevated_permissions) return { text: 'Erweitert', icon: 'check_circle', color: 'var(--color-info)', }
+            return { text: 'Basis', icon: 'key', color: 'var(--color-text-soft)', }
+        }).build(),
+        tableColumnBuilder.new().name('email_verified_at').label('Email bestätigt').transform((value: string | null) => ({
+            text: value ? dayjs(value).fromNow() : 'Ausstehend',
+            tooltip: value ? dayjs(value).format('DD.MM.YYYY HH:mm') : 'Ausstehend',
+            icon: value ? 'check' : 'close',
+            color: value ? 'var(--color-success)' : 'var(--color-error)',
+        })).build(),
+        tableColumnBuilder.new().name('phone_verified_at').label('Telefon bestätigt').transform((value: string | null, item: any) => ({
+            text: value ? dayjs(value).fromNow() : 'Ausstehend',
+            tooltip: value ? dayjs(value).format('DD.MM.YYYY HH:mm') : 'Ausstehend',
+            icon: value ? 'check' : 'close',
+            color: value ? 'var(--color-success)' : 'var(--color-error)',
+        })).build(),
+        tableColumnBuilder.new().name('enabled_at').label('Freigegeben').transform((value: string | null, item: any) => ({
+            text: value ? dayjs(value).fromNow() : 'Ausstehend',
+            tooltip: value ? dayjs(value).format('DD.MM.YYYY HH:mm') : 'Ausstehend',
+            icon: value ? 'check' : 'close',
+            color: value ? 'var(--color-success)' : 'var(--color-error)',
+        })).build(),
+        tableColumnBuilder.new().name('blocked_at').label('Gesperrt').transform((value: string | null, item: any) => ({
+            text: value ? dayjs(value).fromNow() : 'Nein',
+            tooltip: value ? `${dayjs(value).format('DD.MM.YYYY HH:mm')} - Grund: ${item.block_reason || 'Nicht angegeben'}` : 'Nein',
+            icon: value ? 'do_not_disturb_on' : '',
+            color: value ? 'var(--color-error)' : 'var(--color-info)',
+        })).build(),
+        tableColumnBuilder.new().name('has_tfa_enabled').label('Zweiter Faktor').transform((value: boolean | null, item: any) => ({
+            text: value ? 'Aktiv' : 'Inaktiv',
+            tooltip: value ? 'Aktiv' : 'Inaktiv',
+            icon: value ? 'check' : 'close',
+            color: value ? 'var(--color-success)' : 'var(--color-error)',
+        })).build(),
+        tableColumnBuilder.new().name('last_login_at').label('Zuletzt eingeloggt').transform((value: string | null) => ({
+            text: value ? dayjs(value).fromNow() : 'Nie',
+            tooltip: value ? dayjs(value).format('DD.MM.YYYY HH:mm') : 'Nie',
+        })).build(),
+        tableColumnBuilder.new().name('created_at').label('Registriert').transform((value: string | null) => ({
+            text: value ? dayjs(value).fromNow() : '-',
+            tooltip: value ? dayjs(value).format('DD.MM.YYYY HH:mm') : '-',
+        })).build(),
+    ]
+
+    const tableActions = [
+        {
+            icon: 'edit',
+            text: 'Bearbeiten',
+            color: 'var(--color-text)',
+            individual: true,
+            multiple: false,
+            triggerOnRowClick: true,
+            isAvailable: () => true,
+            run: (items: (number | string)[]) => IPM.open(items[0]),
+        },
+        // {
+        //     icon: 'content_copy',
+        //     text: 'Duplizieren',
+        //     color: 'var(--color-text)',
+        //     individual: true,
+        //     multiple: false,
+        //     triggerOnRowClick: false,
+        //     isAvailable: () => true,
+        //     run: (items: (number | string)[]) => IPM.duplicate(items[0]),
+        // },
+        {
+            icon: 'delete',
+            text: 'Löschen',
+            color: 'var(--color-error)',
+            individual: true,
+            multiple: true,
+            triggerOnRowClick: false,
+            isAvailable: () => true,
+            run: (items: (number | string)[]) => IPM.delete(items),
+        },
+    ]
+
+    const tableFilters = ref<FilterSetting[]>([
+        {
+            name: 'roles',
+            label: 'Rollen',
+            type: 'select',
+            multiple: true,
+            values: computed(() => IPM.availableFilterValues['roles']?.map((item: any) => ({ value: item, text: item })) || []),
+        },
+        {
+            name: 'permission_levels',
+            label: 'Berechtigungslevel',
+            type: 'select',
+            multiple: true,
+            values: [
+                { value: 'admin', text: 'Admin' },
+                { value: 'elevated', text: 'Erweitert' },
+            ],
+        },
+        {
+            name: 'email_verified',
+            label: 'Email bestätigt',
+            type: 'select',
+            multiple: false,
+            values: [
+                { value: 'pending', text: 'Ausstehend' },
+                { value: 'active', text: 'Bestätigt' },
+            ],
+        },
+        {
+            name: 'enabled',
+            label: 'Freigegeben',
+            type: 'select',
+            multiple: false,
+            values: [
+                { value: 'pending', text: 'Ausstehend' },
+                { value: 'active', text: 'Freigegeben' },
+            ],
+        },
+        {
+            name: 'tfa_enabled',
+            label: 'Zweiter Faktor',
+            type: 'select',
+            multiple: false,
+            values: [
+                { value: 'inactive', text: 'Inaktiv' },
+                { value: 'active', text: 'Aktiv' },
+            ],
+        },
+    ])
+
+
+
+    // START: Import
+    const importPopup = ref()
+    const importFields = ref([
+        new FieldGroup('Allgemeines', [
+            new Field('username', 'Nutzername'),
+            new Field('email', 'Email'),
+            new Field('password', 'Passwort'),
+            new Field('roles', 'Rollen'),
+        ]),
+        new FieldGroup('Vollständiger Name', [
+            new Field('user_name_salutation', 'Anrede'),
+            new Field('user_name_prefix', 'Titel'),
+            new Field('user_name_firstname', 'Vorname'),
+            new Field('user_name_middlename', 'Zweiter Vorname'),
+            new Field('user_name_lastname', 'Nachname'),
+            new Field('user_name_suffix', 'Suffix'),
+            new Field('user_name_legalname', 'Rechtlicher Name'),
+            new Field('user_name_nickname', 'Spitzname'),
+        ]),
+        new FieldGroup('Organisation', [
+            new Field('user_company_company', 'Organisation'),
+            new Field('user_company_department', 'Abteilung'),
+            new Field('user_company_title', 'Position'),
+        ]),
+    ])
+
+    async function importUsers(data: any[])
+    {
+        await useAxios().post('/api/users/import', {items: data})
+        toast.success('Nutzer wurden importiert')
+        IPM.fetch()
+    }
+    // END: Import
+</script>
+
+<style lang="sass" scoped></style>
